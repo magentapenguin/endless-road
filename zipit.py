5692ffd3a645e5157013bff6b8cfd171c7670c89
magentapenguin <83044109+magentapenguin@users.noreply.github.com>
2025-12-24 14:41:05 -0800

    change from manual building to an action

--- zipit.py
+++
@@ -1,7 +0,0 @@
-import os, shutil
-
-os.system("npm run build")
-
-shutil.make_archive("endless-road", "zip", "dist")
-
-os.system("butler push endless-road.zip magentapenguin/endless-road:browser")
